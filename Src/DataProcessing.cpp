8ee318071e6ebc882f11290de5510925ee8f433d
Kayla Gandy <kayla.gand@gmail.com>
2021-01-07 22:21:54 -0500

    CMake fixes, added to cpp

--- Src/DataProcessing.cpp
+++ Src/DataProcessing.cpp
@@ -5,3 +5,24 @@
 */
 
 #include <DataProcessing.hpp>
+
+twit_data::APIKeys twit_data::APIKeys::from_json(const nlohmann::json& json_containing_keys)
+{
+    if(json_containing_keys.size() < 3)
+    {throw std::runtime_error("Provided JSON incorrectly formatted.");}
+
+    std::string api_key = json_containing_keys.at("API Key");
+    std::string secret_key = json_containing_keys.at("API Secret Key");
+    std::string bearer_token  = json_containing_keys.at("Bearer Token");
+
+    return APIKeys(api_key, secret_key, bearer_token);
+}
+
+twit_data::APIKeys twit_data::parse_keys(const std::string& path_to_json)
+{
+    std::ifstream json_strm(path_to_json);
+    if(!json_strm.good())
+    {throw std::runtime_error("Cannot find JSON given path.");}
+    nlohmann::json key_json = nlohmann::json::parse(json_strm);
+    return APIKeys(key_json);
+}
--- Src/DataProcessing.hpp
+++ Src/DataProcessing.hpp
@@ -6,15 +6,38 @@
 #ifndef TWIT_PROJ_KG
 #define TWIT_PROJ_KG
 
-#include <Twitconfig.h>
 #include <twitcurl.h>
 #include <oauthlib.h>
 #include <nlohmann/json.hpp>
+#include <string>
+#include <fstream>
+#include <iostream>
 
 namespace twit_data{
 
+class APIKeys{
+    std::string _API_key;
+    std::string _API_secret_key;
+    std::string _bearer_token;
+public:
+    APIKeys() = default;
+    explicit APIKeys(const nlohmann::json& json_containing_keys):
+            APIKeys(from_json(json_containing_keys)){}
+    APIKeys(const std::string& API_key, const std::string& API_secret_key, const std::string& bearer_token):
+        _API_key{API_key}, _API_secret_key{API_secret_key}, _bearer_token{bearer_token} {}
+    APIKeys(const APIKeys& keys) = default;
+    APIKeys(APIKeys&& keys) = default;
+    APIKeys& operator=(const APIKeys& keys) = default;
+    APIKeys& operator=(APIKeys&& keys) = default;
+    ~APIKeys() = default;
+    [[nodiscard]] static APIKeys from_json(const nlohmann::json& json_containing_keys);
+};
+
+APIKeys parse_keys(const std::string& path_to_json);
+
 void test_func()
 {
+
     auto twt_obj = twitCurl();
     // twt_obj.getOAuth().setConsumerKey(TWITTER_API_KEY);
     // twt_obj.getOAuth().setConsumerSecret(TWITTER_SECRET_KEY);
